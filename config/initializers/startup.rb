8295e3e606ba65ab48c7f09277371af3c62f2666
Christoph Fabianek <christoph.fabianek@gmail.com>
2023-06-19 09:19:14 +0200

    credential output on startup

---
+++ config/initializers/startup.rb
@@ -0,0 +1,8 @@
+if Doorkeeper::Application.count == 0
+    Doorkeeper::Application.create!({ 
+        name: 'master', 
+        redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
+        scopes: 'admin write read'})
+end
+puts "APP_KEY: " + Doorkeeper::Application.first.uid.to_s
+puts "APP_SECRET: " + Doorkeeper::Application.first.secret.to_s
